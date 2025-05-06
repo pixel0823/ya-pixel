a7498d03f0fa1ec468c63feaada7678cbdf72d4a
jiwoo004 <thswldn0417@naver.com>
2025-05-06 20:52:28 +0900

    MainUI

---
+++ Assets/C#/UI/MainMenu.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickNewGame()
+    {
+
+    }
+
+    public void OnClickLoad()
+    {
+
+    }
+
+    public void OnClickOption()
+    {
+
+    }
+
+    public void OnClickQuit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
