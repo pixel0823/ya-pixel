d59697e86146160d276ce0e90cb2925e9674fa92
im0u22@naver.com <85552569+hppYW@users.noreply.github.com>
2025-06-04 15:05:59 +0900

    06041505
    monster Anim

---
+++ Assets/Script/PlayerStatus.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class PlayerStatus : MonoBehaviour
+{
+    private float PlayerMaxHP = 100;
+    private float PlayerDamage = 10;
+
+    private float PlayCurrentHP;
+
+
+
+    void Awake()
+    {
+        PlayerCurrentHP = PlayerMaxHP;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void GetDamage(float MonsterDamage)
+    {
+        PlayerCurrentHP -= MonsterDamage;
+        // 주인공 맞는 모션
+
+        if (PlayCurrentHP <= 0)
+        {
+            //죽는 모션 넣을거임
+        }
+    }
+
+    public void Attack(float MonsterCurrentHP)
+    {
+        MonsterCurrentHP -= PlayerDamage;
+    }
+}
