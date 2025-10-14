89dedde330a7f10d365ab4ac4ee83660b7e06602
jiwoo004 <thswldn0417@naver.com>
2025-10-14 18:36:45 +0900

    어쩌구

--- Assets/Script/Photon/RoomListItem.cs
+++ Assets/Script/Photon/RoomListItem.cs
@@ -1,14 +1,22 @@
 using UnityEngine;
 using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
 
 public class RoomListItem : MonoBehaviour
 {
     public TMP_Text roomNameText;
     public TMP_Text playerCountText;
     public GameObject privateIcon;
+    
+    private RoomInfo roomInfo;
+    private ConnectionManager connectionManager;
 
-    public void SetRoomInfo(Photon.Realtime.RoomInfo roomInfo)
+    public void Setup(RoomInfo info, ConnectionManager manager)
     {
+        this.roomInfo = info;
+        this.connectionManager = manager;
+
         if (roomNameText == null)
         {
             Debug.LogError("RoomListItem: 'Room Name Text'가 Inspector에 설정되지 않았습니다.", this.gameObject);
@@ -24,14 +32,28 @@ public class RoomListItem : MonoBehaviour
             Debug.LogWarning("RoomListItem: 'Private Icon'이(가) Inspector에 설정되지 않았습니다.", this.gameObject);
         }
 
-        roomNameText.text = roomInfo.Name;
-        playerCountText.text = $"{roomInfo.PlayerCount} / {roomInfo.MaxPlayers}";
-        
+        roomNameText.text = this.roomInfo.Name;
+        playerCountText.text = $"{this.roomInfo.PlayerCount} / {this.roomInfo.MaxPlayers}";
+
         if (privateIcon != null)
         {
-            // "password" 커스텀 프로퍼티가 있고, 그 값이 비어있지 않다면 비공개 방으로 취급
-            bool isPrivate = roomInfo.CustomProperties.ContainsKey("password") && !string.IsNullOrEmpty((string)roomInfo.CustomProperties["password"]);
+            // "password" 커스텀 프로퍼티가 존재하기만 하면 비공개 방으로 취급합니다.
+            bool isPrivate = this.roomInfo.CustomProperties.ContainsKey("password");
             privateIcon.SetActive(isPrivate);
+        }
+    }
+
+    public void OnItemClick()
+    {
+        if (roomInfo.CustomProperties.ContainsKey("password"))
+        {
+            // 비밀번호가 있는 방: ConnectionManager에게 비밀번호 패널을 보여달라고 요청
+            connectionManager.ShowPasswordPanel(roomInfo);
+        }
+        else
+        {
+            // 비밀번호가 없는 방: 바로 입장 시도
+            PhotonNetwork.JoinRoom(roomInfo.Name);
         }
     }
 }
